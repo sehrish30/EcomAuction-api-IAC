4e1bf815fa159a8a5a7c54e568584f55169a534f
sehrish30 <sehrishwaheed98@gmail.com>
2023-05-13 17:57:56 +0300

    fixes

--- auction-service/src/handlers/getSignedUrl.ts
+++ auction-service/src/handlers/getSignedUrl.ts
@@ -22,6 +22,12 @@ const getSignedUrl = async (event: APIGatewayEvent) => {
     const key = uuidv4();
 
     // after 5 minutes signed url will expire
+    /**
+     * Better to deploy this in another service when exepecially ur generating getobject signed url
+     * as this ttl will expire the access to this object
+     * but if u r using cloudfront u need to make sure the cache time also expries before or same time signed url object
+     * cache for security reasons as cloudfront will cache the image for long according to its ttl
+     */
     const expireSeconds = 60 * 5; // 300s = 5 min
 
     // PutObjectCommand Why am I getting this signed url to put object
