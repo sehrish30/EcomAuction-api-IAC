5e516c909d899a05ac7ebdd8078ba7d6c04d0592
sehrish30 <sehrishwaheed98@gmail.com>
2023-05-16 16:14:24 +0300

    updates

---
+++ notes-service/tests/steps/init.js
@@ -0,0 +1,11 @@
+// cognito user pool id, client id, username , password
+// load these values from environment
+
+"use strict";
+
+const init = async () => {
+  // load variables in environment variable process
+  require("dotenv").config();
+};
+
+module.exports = init;
