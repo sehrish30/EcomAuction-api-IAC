0719d9c65b9ebd9448f860f8d3cded8e4349980a
sehrish30 <sehrishwaheed98@gmail.com>
2023-05-21 15:32:03 +0300

    Architecture of api calls to External API

--- notes-service/src/handlers/reportingError.ts
+++ notes-service/src/handlers/reportingError.ts
@@ -1,18 +1,19 @@
 import { APIGatewayEvent } from "aws-lambda";
 // import { log } from "../lib/logger";
-// import * as log from "logging";
+
 // https://github.com/leegilmorecode/serverless-lambda-layers/blob/main/src/generate-screenshot.ts
 // go into the lambda layer folder go to specific file index.ts and run this command "tsc index.ts"
 // include this in tsconfig.json for layers to work,  "include": ["src/**/*.ts", "src/layers/**/node_modules/**/*"]
-import log from "logging";
+const log = require("logging");
+// import log from "logging";
 
 //  { log: { __esModule: true, default: [Function: handler] } }
 // lambda layer e.g for recording metrices , logging
 const reportingError = async (event: APIGatewayEvent) => {
   let data = JSON.parse(event.body as string);
-  // console.log({ logger });
-  // const log = logger?.default;
   console.log({ log });
+  // const log = logger?.default;
+
   try {
     // create note database call
     log({
