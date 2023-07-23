a3f449480accdfe41945d5e1e0a9760b2cbe4732
sehrish30 <sehrishwaheed98@gmail.com>
2023-07-23 13:04:44 +0300

    upload file through s3

--- headlessBrowserTesting/config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    "567562781843-kj2874kmpj2gc3ftmqtndqmq312jr9lr.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI:
-    "mongodb+srv://nasa:nasa@cluster0.h0sil0s.mongodb.net/todoApp?retryWrites=true&w=majority",
-  cookieKey: "123123123",
-  redisUrl: "redis://127.0.0.1:6379",
-};
